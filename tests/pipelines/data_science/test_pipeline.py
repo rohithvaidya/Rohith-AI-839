b66960b2c95698bdbeb185d7ed911e40d350e933
Rohith <rohithvaidya@gmail.com>
2024-09-12 22:54:31 +0530

    pytest for data drift check for three features

--- tests/pipelines/data_science/test_pipeline.py
+++ tests/pipelines/data_science/test_pipeline.py
@@ -1,23 +1,17 @@
-import logging
 
+from evidently.metric_preset import DataDriftPreset
+from evidently.metrics import *
+from evidently.report import Report
+from evidently.tests import *
 import pandas as pd
 import pytest
-from kedro.io import DataCatalog
-from kedro.runner import SequentialRunner
-from rohith_ai_839.pipelines.data_science import create_pipeline as create_ds_pipeline
-from rohith_ai_839.pipelines.data_science.nodes import split_data
+from sklearn.model_selection import train_test_split
+import json
 
 
 @pytest.fixture
 def dummy_data():
-    return pd.DataFrame(
-        {
-            "engines": [1, 2, 3],
-            "crew": [4, 5, 6],
-            "passenger_capacity": [5, 6, 7],
-            "price": [120, 290, 30],
-        }
-    )
+    return pd.read_csv("data/05_model_input/preprocessed_dataset_id_106.csv")
 
 
 @pytest.fixture
@@ -26,49 +20,26 @@ def dummy_parameters():
         "model_options": {
             "test_size": 0.2,
             "random_state": 3,
-            "features": ["engines", "passenger_capacity", "crew"],
+            "features": ["X_1", "X_2", "X_3"],
         }
     }
     return parameters
 
 
 def test_split_data(dummy_data, dummy_parameters):
-    X_train, X_test, y_train, y_test = split_data(
-        dummy_data, dummy_parameters["model_options"]
+    X = dummy_data[dummy_parameters["model_options"]["features"]]
+    y = dummy_data["y"]
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=dummy_parameters["model_options"]["test_size"], random_state=dummy_parameters["model_options"]["random_state"]
     )
-    assert len(X_train) == 2
-    assert len(y_train) == 2
-    assert len(X_test) == 1
-    assert len(y_test) == 1
-
-
-def test_split_data_missing_price(dummy_data, dummy_parameters):
-    dummy_data_missing_price = dummy_data.drop(columns="price")
-    with pytest.raises(KeyError) as e_info:
-        X_train, X_test, y_train, y_test = split_data(
-            dummy_data_missing_price, dummy_parameters["model_options"]
-        )
-
-    assert "price" in str(e_info.value)
-
-
-def test_data_science_pipeline(caplog, dummy_data, dummy_parameters):
-    pipeline = (
-        create_ds_pipeline()
-        .from_nodes("split_data_node")
-        .to_nodes("evaluate_model_node")
+    report = Report(
+        metrics=[
+            DataDriftPreset(),
+        ]
     )
-    catalog = DataCatalog()
-    catalog.add_feed_dict(
-        {
-            "model_input_table": dummy_data,
-            "params:model_options": dummy_parameters["model_options"],
-        }
-    )
+    report.run(reference_data=X_train, current_data=X_test)
+    report_json = json.loads(report.json())
 
-    caplog.set_level(logging.DEBUG, logger="kedro")
-    successful_run_msg = "Pipeline execution completed successfully."
-
-    SequentialRunner().run(pipeline, catalog)
-
-    assert successful_run_msg in caplog.text
+    assert not(report_json["metrics"][1]["result"]["drift_by_columns"]["X_1"]["drift_detected"])
+    assert not(report_json["metrics"][1]["result"]["drift_by_columns"]["X_2"]["drift_detected"])
+    assert not(report_json["metrics"][1]["result"]["drift_by_columns"]["X_3"]["drift_detected"])
