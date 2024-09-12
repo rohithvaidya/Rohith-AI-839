a455fc90fad49f24071b4162a7b82da0366627d9
Rohith <rohithvaidya@gmail.com>
2024-09-12 22:57:53 +0530

    Linted: pytest for data drift check for three features

--- tests/pipelines/data_science/test_pipeline.py
+++ tests/pipelines/data_science/test_pipeline.py
@@ -1,12 +1,12 @@
+import json
 
+import pandas as pd
+import pytest
 from evidently.metric_preset import DataDriftPreset
 from evidently.metrics import *
 from evidently.report import Report
 from evidently.tests import *
-import pandas as pd
-import pytest
 from sklearn.model_selection import train_test_split
-import json
 
 
 @pytest.fixture
@@ -30,7 +30,10 @@ def test_split_data(dummy_data, dummy_parameters):
     X = dummy_data[dummy_parameters["model_options"]["features"]]
     y = dummy_data["y"]
     X_train, X_test, y_train, y_test = train_test_split(
-        X, y, test_size=dummy_parameters["model_options"]["test_size"], random_state=dummy_parameters["model_options"]["random_state"]
+        X,
+        y,
+        test_size=dummy_parameters["model_options"]["test_size"],
+        random_state=dummy_parameters["model_options"]["random_state"],
     )
     report = Report(
         metrics=[
@@ -40,6 +43,12 @@ def test_split_data(dummy_data, dummy_parameters):
     report.run(reference_data=X_train, current_data=X_test)
     report_json = json.loads(report.json())
 
-    assert not(report_json["metrics"][1]["result"]["drift_by_columns"]["X_1"]["drift_detected"])
-    assert not(report_json["metrics"][1]["result"]["drift_by_columns"]["X_2"]["drift_detected"])
-    assert not(report_json["metrics"][1]["result"]["drift_by_columns"]["X_3"]["drift_detected"])
+    assert not (
+        report_json["metrics"][1]["result"]["drift_by_columns"]["X_1"]["drift_detected"]
+    )
+    assert not (
+        report_json["metrics"][1]["result"]["drift_by_columns"]["X_2"]["drift_detected"]
+    )
+    assert not (
+        report_json["metrics"][1]["result"]["drift_by_columns"]["X_3"]["drift_detected"]
+    )
