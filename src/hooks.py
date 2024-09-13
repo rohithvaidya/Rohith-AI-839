a190c5ab045ce35e7e43db30c4449e29db1f732f
Rohith <rohithvaidya@gmail.com>
2024-09-13 16:11:58 +0530

    Serve model using mlflow, api request file uploaded

---
+++ src/hooks.py
@@ -0,0 +1,13 @@
+import mlflow
+
+from kedro.mlflow.hooks import MLflowLoggerHook
+
+
+class CustomMLflowLoggerHook(MLflowLoggerHook):
+    def after_catalog_created(self):
+        mlflow.set_experiment(self.experiment_name)
+
+    def after_node_run(self, node, catalog, inputs, outputs):
+        mlflow.log_params(node.params)
+        mlflow.log_metrics(node.metrics)
+        mlflow.log_artifacts(outputs["predictions"], artifact_path="predictions")
